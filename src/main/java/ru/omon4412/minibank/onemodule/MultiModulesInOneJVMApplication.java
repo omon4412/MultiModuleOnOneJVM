303e3fafeb511ed4bd5244e66f8d5ef9e1f7f17f
Vetrov Sergey <vetrov241201@yandex.ru>
2024-07-01 19:46:12 +0300

    Переделал

--- src/main/java/ru/omon4412/minibank/onemodule/MultiModulesInOneJVMApplication.java
+++ src/main/java/ru/omon4412/minibank/onemodule/MultiModulesInOneJVMApplication.java
@@ -1,14 +1,19 @@
 package ru.omon4412.minibank.onemodule;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import ru.omon4412.minibank.middle.MiddleServiceApplication;
 import ru.omon4412.minibank.telegrambot.TelegramBotApplication;
 
-@SpringBootApplication
-public class MultiModulesInOneJVMApplication {
 
+public class MultiModulesInOneJVMApplication {
     public static void main(String[] args) {
-        SpringApplication.run(new Class[]{MultiModulesInOneJVMApplication.class, MiddleServiceApplication.class, TelegramBotApplication.class}, args);
+        new SpringApplicationBuilder()
+                .sources(MiddleServiceApplication.class)
+                .profiles("middle")
+                .run(args);
+        new SpringApplicationBuilder()
+                .sources(TelegramBotApplication.class)
+                .profiles("telegrambot")
+                .run(args);
     }
 }
